7d07da119ac8d7c23d3fc5e6112c52bcf818c579
skly3900 <skly3900@gmail.com>
2016-08-25 03:58:44 +0900

    :>>>> :)

--- http_inject_level2.py
+++ http_inject_level2.py
@@ -1,13 +1,33 @@
 from scapy.all import *
 
-def backward_in(pkt):
-		packet =pkt.copy()
+def backward_in(packet):
+		reverse_packet =packet.copy()
+		print '************************************'
+		reverse_packet.show()
+		print '************************************'
+		seq_plus=len(packet[Raw].load)
+		print seq_plus
+		print '-------------------'	
+		reverse_packet[Ether].src = packet[Ether].dst
+		reverse_packet[Ether].dst = packet[Ether].src
+		reverse_packet[IP].src = packet[IP].dst
+		reverse_packet[IP].dst = packet[IP].src
+		reverse_packet[TCP].sport = packet[TCP].dport
+		reverse_packet[TCP].dport = packet[TCP].sport
+		reverse_packet[TCP].seq = packet[TCP].ack
+		reverse_packet[TCP].ack = packet[TCP].seq + seq_plus
+		reverse_packet[TCP].flags = 'F'
+		reverse_packet[Raw].load ="Blocked\r\n\r\n"
+		reverse_packet.show()
+		sendp(reverse_packet)
 
-def http_monitor:
+
+def http_monitor(pkt):
 	if pkt.haslayer(Raw):
 		raw_data = str(pkt[Raw].load)
-		if 'GET' int raw_data:
+		if 'GET' in raw_data:
 			pkt.show()
 			backward_in(pkt)
+
 my_ip = '10.211.55.13'
 sniff(prn=http_monitor, filter='host 10.211.55.13 and tcp port 80',store=0)
